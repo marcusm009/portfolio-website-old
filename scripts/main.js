5cd067f051d0ec94a5dd084da9ead483a7d33120
Marcus <marcusm009@gmail.com>
2021-02-24 15:08:31 -0700

    Added htmls for other screens

--- scripts/main.js
+++ scripts/main.js
@@ -13,6 +13,8 @@ let screen;
 
 function init() {
 
+    console.log('VER: 0.01');
+
     initialScreenWidth = window.innerWidth;
     initialScreenHeight = window.innerHeight;
 
@@ -164,8 +166,53 @@ function init() {
 }
 
 window.onload = init;
-
 window.addEventListener('resize', onWindowResize, false);
+
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            console.log('left');
+        } else {
+            console.log('right');
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            console.log('up');
+        } else { 
+            console.log('down');
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
 
 function onWindowResize(){
 
