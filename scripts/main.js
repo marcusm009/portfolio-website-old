add80823cd1786eb5e710013de9209b40bbd9860
Marcus <marcusm009@gmail.com>
2021-02-25 21:17:15 -0700

    Began transforming to new style

--- scripts/main.js
+++ scripts/main.js
@@ -13,7 +13,7 @@ let screen;
 
 function init() {
 
-    console.log('VER: 0.025');
+    console.log('VER: 0.03');
 
     initialScreenWidth = window.innerWidth;
     initialScreenHeight = window.innerHeight;
@@ -49,13 +49,6 @@ function init() {
     // renderer.domElement.style.top = 0;    
     renderer.domElement.style.zIndex = 0;
 
-    // create CSS renderer
-    // cssRenderer = new THREE.CSS3DRenderer();
-    // cssRenderer.setSize(window.innerWidth, window.innerHeight);
-    // cssRenderer.domElement.style.position = 'absolute';
-    // cssRenderer.domElement.style.top = 0;
-    // cssRenderer.domElement.style.zIndex = 1;
-
     // position and point the camera to the center of the scene
     camera.position.set(-1, 4, 1);
     
@@ -69,39 +62,22 @@ function init() {
     // dirLight.position.set(-500, 200, 300);
     dirLight.position.set(-20,100,50);
 
-    // add the output of the renderer to the html element
-    // document.body.appendChild(cssRenderer.domElement);
-    // document.body.appendChild(renderer.domElement);
-
-    let planet = [];
-    for (let i = 0; i < 9; i++) {
-        let ground = new Floor(
-            new THREE.Vector3(0,-i,0),
-            new THREE.Vector2(9,9),
-            new THREE.Vector3(),
-            0.9,
-            0,
-            colors=[0xacff78,0x292929], // light green, blue
-            colorProb=[0,1],
-            holes=[[-3,-3],[3,-3],[3,3]]
-        );
-        planet.push(ground);
-        ground.addToScene(scene);
-    }
+    // add floor
+    let floor = new Floor(
+        new THREE.Vector3(),
+        new THREE.Vector2(9,9),
+        new THREE.Vector3(),
+        0.9,
+        0,
+        colors=[0xacff78,0x292929],
+        colorProb=[0,1],
+        holes=[[-3,-3],[3,-3],[3,3]]
+    )
+    floor.addToScene(scene);
 
     // add player
     let player = new Player(0,0);
     scene.add(player);
-
-    // add screen 0
-    let quaternion = new THREE.Quaternion();
-    // quaternion.setFromAxisAngle( new THREE.Vector3( 0, 1, 0 ), -Math.PI/2);
-    // let screen = new Screen(
-    //     new THREE.Vector3(4.5,3.8,0),
-    //     new THREE.Vector2(9,4.5),
-    //     quaternion
-    // );
-    // screen.addToScene(scene);
 
     // main animation loop
     let frame = 0;
@@ -109,27 +85,12 @@ function init() {
     let playerIsFalling = false;
 
     const animate = () => {
-        // camera.position.set(-1,.5,.75);
-        // cssRenderer.render(scene, camera);
-        // camera.position.multiplyScalar(1000);
         renderer.render(scene, camera);
 
-        player.animate(planet[0]);
+        player.animate(floor);
 
         frame += 1;
         requestAnimationFrame(animate);
-        
-        // TODO: Consider applying quaternion to camera instead
-        if (player.isFalling && camera.position.y > -10) {
-            let vel = .3;
-            focalPoint.y -= vel;
-            camera.lookAt(focalPoint);
-            camera.position.setY(camera.position.y - vel);
-        } else if (player.isFalling) {
-            scrollTransition();
-            player.isFalling = false;
-            player.isDead = true;
-        }
 
         if(frame == 2) {
             window.scrollTo(0,0);
@@ -144,17 +105,16 @@ function init() {
 
     animate();
 
-    $(window).scroll(() => {
-        let scroll = $(window).scrollTop();
-        let transparency = Math.min(Math.max(scroll/window.innerHeight, 0.5), 1);
-        let redness = Math.min(Math.max(scroll/window.innerHeight*255, 64), 215);
-        // console.log(scroll);
-        console.log(transparency);
-        console.log(redness);
-        // $('#nav-bar').css('background-color', `rgba(${redness}, 64, 64, ${transparency})`);
-        $('#nav-bar').css('background-color', `rgba(64, 64, 64, ${transparency})`);    
-
-    });
+    // $(window).scroll(() => {
+    //     let scroll = $(window).scrollTop();
+    //     let transparency = Math.min(Math.max(scroll/window.innerHeight, 0.5), 1);
+    //     let redness = Math.min(Math.max(scroll/window.innerHeight*255, 64), 215);
+    //     // console.log(scroll);
+    //     console.log(transparency);
+    //     console.log(redness);
+    //     // $('#nav-bar').css('background-color', `rgba(${redness}, 64, 64, ${transparency})`);
+    //     $('#nav-bar').css('background-color', `rgba(64, 64, 64, ${transparency})`);    
+    // });
 
     $('#title').click(() => {
         location.reload();
@@ -174,7 +134,6 @@ function onWindowResize(){
     camera.updateProjectionMatrix();
 
     renderer.setSize(window.innerWidth, window.innerHeight);
-    // cssRenderer.setSize(window.innerWidth, window.innerHeight);
 
     console.log(screen.scale);
     screen.scale.setX(window.innerWidth/initialScreenWidth);
